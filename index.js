59be49dc33924f8f216deb0b9dfb8cabea60b486
Nathan Zadoks <nathan@nathan7.eu>
2013-08-13 17:57:45 +0200

    actually add a request body

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports = function(req, res, next) {
   res.statusCode = 301
   res.setHeader('Content-Type', 'text/plain')
   res.setHeader('Location', 'https://' + req.headers.host + req.url)
-  res.end()
+  res.end('redirecting you to the secure site')
 }
